9b718947d38bd92dd599b8f98086bb3803d9f758
ramialkaro <almemari22ramosh@gmail.com>
2020-09-03 17:03:27 +0300

    using generics instead of apiview

--- src/core/views.py
+++ src/core/views.py
@@ -2,14 +2,32 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # third-party imports
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework import generics
 from .serializers  import PostSerializers
 from .models import Post
 
-class TestView(APIView):
+
+class PostView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin, 
+    generics.GenericAPIView):
+    serializer_class = PostSerializers
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+
+
+""" class TestView(APIView):
 
     permission_classes = (IsAuthenticated, )
 
@@ -25,4 +43,4 @@ class TestView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors)
+        return Response(serializer.errors) """
