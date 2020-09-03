a82f57e28d037565fb0b26684cda94759644c93d
ramialkaro <almemari22ramosh@gmail.com>
2020-09-03 14:25:36 +0300

    implement registration using the rest-auth to project

--- src/drf_api/urls.py
+++ src/drf_api/urls.py
@@ -8,6 +8,7 @@ from core.views import  TestView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('',TestView.as_view(), name='test'),
     path('api/token',obtain_auth_token, name ='obtain-token')
